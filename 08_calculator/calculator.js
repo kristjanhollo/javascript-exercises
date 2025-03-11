89f642402e303af0abcd905b1c86062a62d47088
Kristjan Hollo <hollo.kristjan@gmail.com>
2025-03-11 21:16:39 +0200

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+  if (arguments[0] < 0 && arguments[1] < 0) {
+    return arguments[0] - arguments[1];
+  }
+	return arguments[0] - arguments[1];
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+	return arguments[0].reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  return arguments[0].reduce((a, b) => a * b, 1);
 };
 
 const power = function() {
-	
+	return Math.pow(arguments[0], arguments[1]);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  }
+  for (let i = number - 1; i >= 1; i--) {
+    number *= i;
+  }
+  return number;
+
 };
 
 // Do not edit below this line
