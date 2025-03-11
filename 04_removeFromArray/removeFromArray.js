c18a793a4d0478612cdfe943e0d424d88e5012ef
Kristjan Hollo <hollo.kristjan@gmail.com>
2025-03-11 20:54:41 +0200

    04_removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
+    let arr = args.shift();
+    let newArr = arr.filter((ele) => !args.includes(ele));
+    return newArr;
+    
 };
 
 // Do not edit below this line
