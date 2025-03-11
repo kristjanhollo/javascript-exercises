c18644a7eb63d9f1cf000d5058f3a5ac4f449efc
Kristjan Hollo <hollo.kristjan@gmail.com>
2025-03-11 21:00:35 +0200

    05_sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number' || Number.isInteger(num1) === false || Number.isInteger(num2) === false) {
+        return 'ERROR';
+    }
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+    let sum = 0;
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
